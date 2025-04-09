d83c152e8c611161159d40aceee8817acfde9a7d
gowtham <raju@gmail.com>
2025-04-09 14:18:41 +0530

    all

--- catalogue.sh
+++ catalogue.sh
@@ -1,21 +1,22 @@
 ID=$(id -u)
-$R="\e[31m"
-$G="\e[32m"
-$Y="\e[33m"
-$N="\e[0m"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
-TIMESTAMP=$(date +%F-%H_%M_%S)
+TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 echo "script started executing at $TIMESTAMP" &>> $LOGFILE
 
-VALIDATE{}{
-    if [$1 -ne 0]
-    then 
-        echo -e "$2 ....$R FAILED $N"
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+        exit 1
     else
-        echo -e "$2 ...  $G SUCESS $N"
-    fi        
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
 }
   
 if [ID -ne 0]
